7cbc191988f270d1b86eba5edccb5bf2dafc8920
Jakob Voss <voss@gbv.de>
2026-03-18 11:04:24 +0100

    Fix section numbers

--- implementation/python.py
+++ implementation/python.py
@@ -5,10 +5,7 @@ def mapping_sameness_identifier(mapping):
     subjects = "|".join(sorted(mapping["subjects"]))
     objects = "|".join(sorted(mapping["objects"]))
     string = f"{subjects} {mapping['predicate']} {objects}"
-
-    print(string)
     sha = hashlib.sha256(string.encode('utf-8'))
-    print(sha.hexdigest())
     id = base64.urlsafe_b64encode(sha.digest()).decode('ascii').rstrip('=')
     return f"mapping:~{id}" if mapping["negative"] else f"mapping:{id}"
 
